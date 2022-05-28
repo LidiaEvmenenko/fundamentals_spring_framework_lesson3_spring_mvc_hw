b6f5e6d8758481c793da553fa42f6be540b8ac3b
LidiaEvmenenko <inu-chan@mail.ru>
2022-05-28 16:18:45 +0500

    Initial commit

---
+++ src/main/java/ru/geekbrains/fundamentals_spring_framework_lesson3_spring_mvc_hw/MainApp.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.fundamentals_spring_framework_lesson3_spring_mvc_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/fundamentals_spring_framework_lesson3_spring_mvc_hw/MainAppTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.fundamentals_spring_framework_lesson3_spring_mvc_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
